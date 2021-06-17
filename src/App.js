65d063eeb60ebe1d581128d13ceafaa4dfe69379
psunil4 <sd.panchal93@gmail.com>
2021-06-16 20:29:39 -0700

    DONE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import styled from 'sty'
-import './App.css';
+import { Provider as ReduxProvider } from "react-redux";
+import DarkThemeProvider from "./components/DarkThemeProvider";
+import DarkThemeToggle from "./components/DarkThemeToggle"
+import { Container } from "./utils";
+import store from "./store";
+
+import "./App.css";
 
 function App() {
   return (
-
+    <>
+      <ReduxProvider store={store}>
+        <DarkThemeProvider>
+         <DarkThemeToggle/>
+          <Container>
+            <h2>Welcome!</h2>
+            <h3>Full Stack Web Development</h3>
+          </Container>
+        </DarkThemeProvider>
+      </ReduxProvider>
+    </>
   );
 }
 
